761ac319ed2971d6cd4ae7ad3ee613ce5ea8e5ad
avireni <beena@avireni.com>
2015-06-26 11:19:02 -0400

    updated styles

---
+++ 2/scripts/login.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$("#userLogin").click(function(event) {
+		window.location.href = "home.html";
+		event.preventDefault();
+
+		// validations here
+		// if ($("#username").val()=== "") {
+  //               $("#message").html("Please enter the User Name");
+  //               return false;
+  //               }
+        
+  //               if ($("#password").val()=== "") {
+  //                       $("#message").html("Please enter the Password");
+  //                       return false;
+  //               }
+        
+		// ajax call
+		// $.ajax ({
+		// 	type: 'POST',
+		// 	url: 'login.php',
+		// 	data: $(this).serialize(),
+		// 	dataType: 'json',
+		// 	success: function (data) {
+		// 			console.log(data);
+		// 			if (data.msg === "happened"){
+		// 			   //$('#message').html("Success Login");
+  //                                          window.location.href = "home.php";
+		// 			}
+	 //                               else {
+	 //                                       $("#message").html("Login failed. Provide correct login details");
+	 //                               }
+		// 		},
+  //                       error: function(data){
+	 //              	$("#message").html("your ajax call failed."); 
+	 //                }
+		// });
+	});
+});
